598cba64d667003453a7abd1380eebe31d7d280e
Swapnil Singh <mail@swapnilsingh.me>
2021-05-15 00:58:58 +0530

    Adds svelte, rescript, tailwind

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { ping } from './ping.bs';
+	let name = 'world';
+</script>
+
+<h1>Hello {ping()}!</h1>
+
+<style global lang="postcss">
+  @tailwind base;
+  @tailwind components;
+  @tailwind utilities;
+</style>
+
---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+  future: {
+    purgeLayersByDefault: true,
+    removeDeprecatedGapUtilities: true,
+  },
+  purge: {
+    content: [
+      "./src/**/*.svelte",
+    ],
+    enabled: true
+  },
+}
